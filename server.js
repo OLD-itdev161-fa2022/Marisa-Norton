4648f39be21718e977939c6f22c5ba54dee3ea62
Marisa Norton <nortonm5@gmatc.matc.edu>
2021-09-15 13:20:26 -0500

    updated a2

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import connectDatabase from './config/db';
+
 
 
 
@@ -6,10 +8,13 @@ import express from 'express';
 const app = express();
 
 
+//connect database
+connectDatabase();
+
 //API endpoints
 app.get('/', (req, res) =>
  res.send('http get request sent to root api endpoint')
 );
 
 //connection listener
-app.listen(3000, () => console.log(`Express server running on port ${3000}`));
+app.listen(3000, () => console.log(`Express server running on port 3000`));
