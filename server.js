c250a831f44dd6fccd8499b612e771c090b28b13
Marisa Norton <marisanorton@mac-mini.home>
2021-09-22 17:20:55 -0500

    updated server.js file

--- server.js
+++ server.js
@@ -9,10 +9,25 @@ const app = express();
 //connect database
 connectDatabase();
 
+//Configure middleware
+app.use(express.json({ extended: false }));
+
+
 //API endpoints
 app.get('/', (req, res) =>
  res.send('http get request sent to root api endpoint')
 );
+
+/** 
+* @route POST api/users
+* @desc Test Endpoint
+*/
+
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
+
 
 //connection listener
 app.listen(3000, () => console.log(`Express server running on port 3000`));
