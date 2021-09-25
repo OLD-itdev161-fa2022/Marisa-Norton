b2b81d0441f843cd07e07acb4d4c7a2e34f881de
Marisa Norton <marisanorton@mac-mini.home>
2021-09-25 17:58:19 -0500

    completed Activity 3

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDatabase from './config/db';
+import { check, validationResult } from 'express-validator';
 
 
 //initialize express application
@@ -23,10 +24,27 @@ app.get('/', (req, res) =>
 * @desc Test Endpoint
 */
 
-app.post('/api/users', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-});
+app.post('/api/users',
+    [
+    check('name', 'Please enter your name')
+        .not()
+        .isEmpty(),
+    check('email', 'Please enter a valid emial')
+        .isEmail(),
+    check(
+        'password', 
+        'Please enter a password with 6 or more characters')
+        .isLength({ min:6})
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }else {
+            return res.send(req.body);
+        }
+    }
+);
 
 
 //connection listener
